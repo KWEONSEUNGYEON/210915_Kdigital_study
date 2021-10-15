46981176753eecbc6987a59e444a1e67a667cfa9
KWEONSEUNGYEON <ddunyeon@naver.com>
2021-10-15 14:51:15 +0900

    20211015

--- java_work/211015/src/bb/C.java
+++ java_work/211015/src/bb/C.java
@@ -3,7 +3,7 @@ package bb;
 public class C {
 
 	public static void main(String[] args) {
-		System.out.println(3147483647+3147483647);
+//		System.out.println(3147483647+3147483647);
 		System.out.println(3147483647L+3147483647L);
 	}
 }
---
+++ py/211015/a.py
@@ -0,0 +1,5 @@
+def greet():
+    print("파일로실행")
+    print("ㅎㅎㅎ...")
+
+greet()
