08f26a1833c396184e7f889bfa076ac7b0b3d217
KWEONSEUNGYEON <ddunyeon@naver.com>
2022-02-16 11:06:09 +0900

    up

---
+++ CSharp/20220216/MyCSharp_Chap10/MyCSharp_Chap10/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCSharp_Chap10
+{
+    public partial class Form2 : Form, IObserver
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(ISubject sub)
+        {
+            InitializeComponent();
+            sub.registerObserver(this);
+        }
+
+        public void update(string value)
+        {
+            textBox1.Text = value;
+        }
+    }
+}
