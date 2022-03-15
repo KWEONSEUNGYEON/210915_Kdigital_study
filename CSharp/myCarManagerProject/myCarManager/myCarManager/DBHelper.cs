5a48571110e270d7c6fd540ed96377fae0cd3721
KWEONSEUNGYEON <ddunyeon@naver.com>
2022-03-15 11:00:12 +0900

    up

---
+++ CSharp/myCarManagerProject/myCarManager/myCarManager/DBHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCarManager
+{
+    public class DBHelper
+    {
+        private static SqlConnection conn = new SqlConnection();
+        public static SqlDataAdapter da;
+        public static DataSet ds;
+        public static DataTable dt;
+
+        private static void ConnectDB()
+        {
+            conn.ConnectionString = string.Format("Data Source=({0}); " +
+                "initial Catalog={1};" +
+                "integrated Security={2};" +
+                "Timeout=3",
+                "local", "MyCarManagerDB", "SSPI");
+            conn = new SqlConnection(conn.ConnectionString);
+            conn.Open();
+        }
+    }
+}
---
+++ CSharp/myCarManagerProject/myCarManager/myCarManager/DataManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCarManager
+{
+    public class DataManager
+    {
+        public static List<ParkingCar> Cars = new List<ParkingCar>();
+
+        static DataManager()
+        {
+            Load();
+        }
+
+        public static void Load()
+        {
+
+        }
+        public static void Save() //주차 출차때 주로 사용
+        {
+
+        }
+        public static void PrintLog(string contents)
+        {
+            //폴더 명이 ParkingHistory인 것을 찾는 중
+            DirectoryInfo di = new DirectoryInfo("ParkingHistory");
+
+            if(!di.Exists)
+            {
+                di.Create();
+            }
+
+            using(StreamWriter Writer = new StreamWriter("ParkingHistory\\ParkingHistory.txt", true))
+            {
+                Writer.WriteLine(contents);
+            }
+        }
+    }
+}
